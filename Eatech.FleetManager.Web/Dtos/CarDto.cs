64d3f086e405e0762aea9ee8c8cf98381cfd801e
haihala <eero.haihala@gmail.com>
2018-02-28 15:02:15 +0200

    made a shorthand constructor for CarDto from Car

--- Eatech.FleetManager.Web/Dtos/CarDto.cs
+++ Eatech.FleetManager.Web/Dtos/CarDto.cs
@@ -11,5 +11,14 @@ namespace Eatech.FleetManager.ApplicationCore.Entities
         public string Model { get; set; }
 
         public string Manufacturer { get; set; }
+
+        public CarDto (Car car)
+        {
+            // Makes CarController a bit neater.
+            Id = car.Id;
+            ModelYear = car.ModelYear;
+            Model = car.Model;
+            Manufacturer = car.Manufacturer;
+        }
     }
 }
