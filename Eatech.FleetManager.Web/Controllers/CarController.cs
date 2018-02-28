53e99d2eb4357704add599af66a622aed2686d3f
haihala <eero.haihala@gmail.com>
2018-02-28 15:06:05 +0200

    implemented adding and updating

--- Eatech.FleetManager.Web/Controllers/CarController.cs
+++ Eatech.FleetManager.Web/Controllers/CarController.cs
@@ -24,13 +24,7 @@ namespace Eatech.FleetManager.Web.Controllers
         [HttpGet]
         public async Task<IEnumerable<CarDto>> Get()
         {
-            return (await _carService.GetAll()).Select(car => new CarDto
-            {
-                Id = car.Id,
-                ModelYear = car.ModelYear,
-                Model = car.Model,
-                Manufacturer = car.Manufacturer
-            });
+            return (await _carService.GetAll()).Select(car => new CarDto (car));
         }
 
         /// <summary>
@@ -45,13 +39,38 @@ namespace Eatech.FleetManager.Web.Controllers
                 return NotFound();
             }
 
-            return Ok(new CarDto 
+            return Ok(new CarDto(car));
+        }
+        /// <summary>
+        ///     Example HTTP POST: api/car/570890e2-8007-4e5c-a8d6-c3f670d8a9be
+        ///     Example adds a car with id 570890e2-8007-4e5c-a8d6-c3f670d8a9be, null year, null model and null manufacturer.
+        /// </summary>
+        [HttpPost("{id}")]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            // Some authentication should take place in the final product.
+            // Sanitation too
+            var car = await _carService.Get(id);
+            if (car == null)
             {
-                Id = car.Id,
-                ModelYear = car.ModelYear,
-                Model = car.Model,
-                Manufacturer = car.Manufacturer
-            });
+                // Car with this id doesn't exist, add.
+                return Ok(new CarDto(await _carService.Add(
+                    id,
+                    ModelYear: (int?)Int32.Parse(Request.Form["ModelYear"].ToString()),
+                    Model: Request.Form["Model"],
+                    Manufacturer: Request.Form["Manufacturer"]
+                    )));
+            }
+            else
+            {
+                // Car with this id exists, update with given info
+                return Ok(new CarDto(await _carService.Update(
+                    id,
+                    ModelYear: (int?)Int32.Parse(Request.Form["ModelYear"].ToString()),
+                    Model: Request.Form["Model"],
+                    Manufacturer: Request.Form["Manufacturer"]
+                    )));
+            }
         }
     }
 }
