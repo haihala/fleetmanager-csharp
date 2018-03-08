1cd625d834123ada1930a0179685a3c5b545e756
haihala <eero.haihala@gmail.com>
2018-03-08 18:28:59 +0200

    added more and fixed existing unit tests

--- Eatech.FleetManager.UnitTests/ApplicationCore/Services/CarServiceTests/CarGet.cs
+++ Eatech.FleetManager.UnitTests/ApplicationCore/Services/CarServiceTests/CarGet.cs
@@ -17,14 +17,28 @@ namespace Eatech.FleetManager.UnitTests.ApplicationCore.Services.CarServiceTests
 
             Assert.NotNull(cars);
             Assert.NotEmpty(cars);
-            Assert.Equal(2, cars.Count);
+        }
+
+        [Fact]
+        public async void FilteredAllCars()
+        {
+            ICarService carService = new CarService();
+
+            var cars = (await carService.GetAll(
+                minYear: 1000,
+                maxYear: 3000,
+                model: "model7",
+                manufacturer: "make10")).ToList();
+
+            Assert.NotNull(cars);
+            Assert.NotEmpty(cars);
         }
 
         [Fact]
         public async void ExistingCardWithId()
         {
             ICarService carService = new CarService();
-            var carId = Guid.Parse("d9417f10-5c79-44a0-9137-4eba914a82a9");
+            var carId = Guid.Parse("231dcb10-d3ee-4687-aa4a-25f507dc340b");
 
             var car = await carService.Get(carId);
 
---
+++ Eatech.FleetManager.UnitTests/ApplicationCore/Services/CarServiceTests/CarSet.cs
@@ -0,0 +1,88 @@
+﻿using Eatech.FleetManager.ApplicationCore.Entities;
+using Eatech.FleetManager.ApplicationCore.Interfaces;
+using Eatech.FleetManager.ApplicationCore.Services;
+using System;
+using Xunit;
+
+namespace Eatech.FleetManager.UnitTests.ApplicationCore.Services.CarServiceTests
+{
+    public class CarSet
+    {
+        [Fact]
+        public async void AddRemoveCar()
+        {
+            // Adding and removing should be done in the same test, since that allows for tests that don't mutate the data.
+            ICarService carService = new CarService();
+
+            Guid id = Guid.Parse("d9417f10-1111-1111-1111-4eba914a82a9");
+            bool existsBefore = await carService.Get(id) != null;
+            await carService.Update(id);
+            bool existsMiddle = await carService.Get(id) != null;
+            await carService.Remove(id);
+            bool existsAfter = await carService.Get(id) != null;
+
+            Assert.False(existsBefore);
+            Assert.True(existsMiddle);
+            Assert.False(existsAfter);
+        }
+
+        [Fact]
+        public async void AddRemoveEqualCar()
+        {
+            // Add a new car for testing and change it.
+            ICarService carService = new CarService();
+
+            Guid id = Guid.Parse("d9417f10-1111-1111-1111-4eba914a82a9");
+            Car car1 = await carService.Update(id);
+            Car car2 = await carService.Remove(id);
+            
+            Assert.Equal(car1, car2);
+        }
+
+        [Fact]
+        public async void ChangeCar()
+        {
+            // Add a new car for testing and change it.
+            ICarService carService = new CarService();
+
+            Guid id = Guid.Parse("d9417f10-1111-1111-1111-4eba914a82a9");
+            Car car1 = await carService.Update(id);
+            Car car2 = await carService.Update(id, 
+                modelYear: 1999);
+            Car car3 = await carService.Remove(id);
+
+            Assert.NotEqual(car1, car2);
+            Assert.Equal(car2, car3);
+        }
+        [Fact]
+        public async void FakeChangeCar()
+        {
+            // Change car to what it already is. Cars should equal.
+            ICarService carService = new CarService();
+
+            Guid id = Guid.Parse("d9417f10-1111-1111-1111-4eba914a82a9");
+            Car car1 = await carService.Update(id,
+                modelYear: 2000,
+                model: "model",
+                manufacturer: "maker",
+                registration: "abc-123",
+                inspectionDate: DateTime.Parse("2018-05-08T00:00:00"),
+                engineSize: 100,
+                enginePower: 42);
+            Car car2 = await carService.Update(id,
+                modelYear: 2000,
+                model: "model",
+                manufacturer: "maker",
+                registration: "abc-123",
+                inspectionDate: DateTime.Parse("2018-05-08T00:00:00"),
+                engineSize: 100,
+                enginePower: 42);
+            Car car3 = await carService.Remove(id);
+
+            Assert.Equal(car1, car2);
+            Assert.Equal(car2, car3);
+        }
+    }
+
+
+}
